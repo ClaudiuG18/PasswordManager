5920d2f5326b14d4f35f7903841a364aff1ffd2a
Claudiu Ghise <cghise@memmert.com>
2023-05-16 15:24:41 +0200

    added  canvas, label website, label email and label password

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20,pady=20)
+
+#Canvas
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(column=1, row=0)
+
+#Labels
+l_website = Label(text="Website:")
+l_website.grid(column=0, row=1, sticky="w" )
+
+l_email_username = Label(text="Email/Username:")
+l_email_username.grid(column=0, row=2)
+
+l_password = Label(text="Password:")
+l_password.grid(column=0, row=3, sticky="w")
+
+
+
+
+
+
+window.mainloop()
