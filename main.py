a56a1d3d9bc1660d1eb7b52acfa28ab541066504
Claudiu Ghise <cghise@memmert.com>
2023-05-17 13:12:07 +0200

    canvas padding changed

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ window.config(padx=20,pady=20)
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=1, row=0)
+canvas.grid(column=0, row=0, columnspan=3)
 
 #Labels
 l_website = Label(text="Website:")
